e2c1d01a0cfaac1523bbbbc9c098f59a7ed4aeb0
Tony Rodriguez <tony.rodriguez@gmail.com>
2026-09-03 11:55:44 -0700

    feat: redesign footer for bay area air district

--- web/components/shell/page-footer.tsx
+++ web/components/shell/page-footer.tsx
@@ -1,106 +1,140 @@
-import { StandardFooter, LegalFooter, AbagLogo, MtcLogo, ExternalLink, Typography } from '@bayareametro/mtc-ui';
+import { StandardFooter } from '@bayareametro/mtc-ui';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faXTwitter, faLinkedin, faFacebookSquare, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
-import packageJson from '../../package.json';
+import {
+  faXTwitter,
+  faFacebookSquare,
+  faYoutube,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import styles from './page-footer.module.scss';
 
 export const PageFooter = () => (
-  <StandardFooter.Root>
-    <StandardFooter.Container>
-      <StandardFooter.Row>
-        <StandardFooter.Col>
-          <ExternalLink
-            href="https://mtc.ca.gov"
-            title="Metropolitan Transportation Commission"
-            RightAdornment={null}
+  <StandardFooter.Root className={styles.root}>
+    <div className={styles.container}>
+      <div className={styles.grid}>
+        {/* Column 1: Contact & Logo */}
+        <div className={styles.column}>
+          <a
+            href="https://www.baaqmd.gov/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.logoLink}
+            title="Bay Area Air Quality Management District Website"
           >
-            <StandardFooter.Logo as={MtcLogo} />
-          </ExternalLink>
-          <Typography variant="paragraph2Emphasis">Metropolitan Transportation Commission</Typography>
-          <Typography>
-            MTC is responsible for planning, financing and coordinating transportation for the
-            nine-county San Francisco Bay Area.
-          </Typography>
-          <StandardFooter.ExternalLink href="https://mtc.ca.gov">mtc.ca.gov</StandardFooter.ExternalLink>
-          <StandardFooter.SocialsContainer>
-            <StandardFooter.SocialIconLink
-              href="https://www.twitter.com/mtcbata"
-              title="Find Metropolitan Transportation Commission on Twitter"
-            >
-              <FontAwesomeIcon icon={faXTwitter} size="lg" />
-            </StandardFooter.SocialIconLink>
-            <StandardFooter.SocialIconLink
-              href="https://www.linkedin.com/company/metropolitan-transportation-commission"
-              title="Find Metropolitan Transportation Commission on LinkedIn"
-            >
-              <FontAwesomeIcon icon={faLinkedin} size="lg" />
-            </StandardFooter.SocialIconLink>
-            <StandardFooter.SocialIconLink
-              href="https://www.facebook.com/MTCBATA"
-              title="Find Metropolitan Transportation Commission on Facebook"
-            >
-              <FontAwesomeIcon icon={faFacebookSquare} size="lg" />
-            </StandardFooter.SocialIconLink>
-            <StandardFooter.SocialIconLink
-              href="https://www.instagram.com/mtcbata"
-              title="Find Metropolitan Transportation Commission on Instagram"
-            >
-              <FontAwesomeIcon icon={faInstagram} size="lg" />
-            </StandardFooter.SocialIconLink>
-            <StandardFooter.SocialIconLink
-              href="https://www.youtube.com/user/mtcabaglibrary"
-              title="Find Metropolitan Transportation Commission on YouTube"
-            >
-              <FontAwesomeIcon icon={faYoutube} size="lg" />
-            </StandardFooter.SocialIconLink>
-          </StandardFooter.SocialsContainer>
-        </StandardFooter.Col>
+            <img
+              src="/images/HorizLogo-WHT.png"
+              alt="Bay Area Air Quality Management District Logo"
+              className={styles.logo}
+            />
+          </a>
+          <p className={styles.districtTitle}>Bay Area Air District</p>
+          <address className={styles.address}>
+            375 Beale Street, Suite 600
+            <br />
+            San Francisco, CA 94105
+          </address>
+          <p className={styles.contactLine}>
+            <a href="tel:1-415-749-5000">415.749.5000 | 1.800.HELP AIR</a>
+          </p>
+          <a
+            href="https://www.baaqmd.gov/about-the-air-district/contact-us/location-and-directions"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.directionsLink}
+          >
+            <FontAwesomeIcon icon={faLocationDot} aria-hidden="true" />
+            <span>Directions</span>
+          </a>
+        </div>
 
-        <StandardFooter.Col>
-          <ExternalLink href="https://abag.ca.gov/" title="Association of Bay Area Governments" RightAdornment={null}>
-            <StandardFooter.Logo as={AbagLogo} />
-          </ExternalLink>
-          <Typography variant="paragraph2Emphasis">Association of Bay Area Governments</Typography>
-          <Typography>
-            ABAG supports regional planning and cooperation among the cities and counties of the San
-            Francisco Bay Area.
-          </Typography>
-          <StandardFooter.ExternalLink href="https://abag.ca.gov">abag.ca.gov</StandardFooter.ExternalLink>
-        </StandardFooter.Col>
+        {/* Column 2: Stay Informed */}
+        <div className={styles.column}>
+          <h2 className={styles.heading}>Stay Informed</h2>
+          <div className={styles.socials}>
+            <a
+              href="https://twitter.com/AirDistrict"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.socialLink}
+              title="Follow Bay Area Air District on X / Twitter"
+              aria-label="X / Twitter"
+            >
+              <FontAwesomeIcon icon={faXTwitter} />
+            </a>
+            <a
+              href="https://www.facebook.com/bayareaairdistrict"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.socialLink}
+              title="Follow Bay Area Air District on Facebook"
+              aria-label="Facebook"
+            >
+              <FontAwesomeIcon icon={faFacebookSquare} />
+            </a>
+            <a
+              href="https://www.youtube.com/user/BayAreaAirDistrict"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.socialLink}
+              title="Watch Bay Area Air District videos on YouTube"
+              aria-label="YouTube"
+            >
+              <FontAwesomeIcon icon={faYoutube} />
+            </a>
+            <a
+              href="https://www.instagram.com/bayareaairdistrict"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.socialLink}
+              title="Follow Bay Area Air District on Instagram"
+              aria-label="Instagram"
+            >
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+          </div>
+          <a
+            href="https://www.baaqmd.gov/about-the-air-district/stay-informed/subscriptions"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.subscribeLink}
+          >
+            Subscribe
+          </a>
+        </div>
 
-        <StandardFooter.Col>
-          <StandardFooter.ContactDetails>
-            <Typography variant="paragraph1" color="var(--dv-gold)">
-              Contact Us
-            </Typography>
-            <Typography variant="paragraph4">
-              Bay Area Metro Center
-              <br />
-              375 Beale Street
-              <br />
-              San Francisco, CA 94105
-            </Typography>
-            <Typography variant="paragraph4">
-              Main Phone Number: <a href="tel:1-415-778-6700">(415) 778-6700</a>
-              <br />
-              Public Information Line: <a href="tel:1-415-778-6757">(415) 778-6757</a>
-            </Typography>
-            <Typography variant="paragraph4">
-              <ExternalLink href="mailto:info@bayareametro.gov">info@bayareametro.gov</ExternalLink>
-            </Typography>
-          </StandardFooter.ContactDetails>
-        </StandardFooter.Col>
-      </StandardFooter.Row>
+        {/* Column 3: Air District Initiatives */}
+        <div className={styles.column}>
+          <h2 className={styles.heading}>Air District Initiatives</h2>
+          <div className={styles.initiatives}>
+            <a
+              href="https://www.sparetheair.org"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.initiativeLink}
+              title="Spare the Air"
+            >
+              <img
+                src="/images/spare_the_air.png"
+                alt="Spare the Air"
+                className={styles.initiativeImg}
+              />
+            </a>
+            <div className={styles.noBurnSection}>
+              <p className={styles.phoneText}>877-4NO-BURN</p>
+              <img
+                src="/images/no_burn.png"
+                alt="Wood Smoke Regulations / No Burn"
+                className={styles.initiativeImg}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
 
-      <Typography variant="paragraph4" style={{ textAlign: 'center', width: '100%' }}>
-        &copy; {new Date().getFullYear()} Metropolitan Transportation Commission
-      </Typography>
-
-      <LegalFooter.Root>
-        <LegalFooter.Container>
-          <span style={{ color: '#80979f' }}>Version {packageJson.version}</span>
-        </LegalFooter.Container>
-        <LegalFooter.BackToTopLink />
-      </LegalFooter.Root>
-    </StandardFooter.Container>
+      <footer className={styles.bottomBar}>
+        <span>&copy; {new Date().getFullYear()} Bay Area Air District. All rights reserved.</span>
+      </footer>
+    </div>
   </StandardFooter.Root>
 );
